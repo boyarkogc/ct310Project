61384b68883ef900c55d9e3d351d7af901707fdc
Greg Boyarko <boyarkogc@gmail.com>
2016-02-27 17:18:29 -0700

    initial header and footer

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+	<h6>This site is part of a CSU CT 310 Course Project." The text "CT 310" will be a link to the course homepage</h6>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<!-- Start of page Body -->
+<body>
+	<div class="header">
+		<p>Header</p>
+	</div>
