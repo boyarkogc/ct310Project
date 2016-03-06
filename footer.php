18fe97f528850fc503f4535d6b0825d5466fe12d
Greg Boyarko <boyarkogc@gmail.com>
2016-03-06 12:33:16 -0700

    finally got the css loading properly

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <footer>
-	<h6><?php if (isset($_SESSION['startTime'])) { echo "You are currently logged in as " . $_SESSION['userName']; } ?></h6>
+	<h6><?php if (isset($_SESSION['startTime'])) { 
+		echo "You are currently logged in as " . $_SESSION['userName']; }
+		else { echo "You are not currently logged in"; } ?></h6>
 	<h6>This site is part of a CSU CT 310 Course Project." The text "CT 310" will be a link to the course homepage</h6>
 </footer>
 </body>
--- header.php
+++ header.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link href="style.css" rel="stylesheet" type="text/css" />
-	<header>
-			<div id= "top">
-				<h1>J &amp; G Animal Rescue and Adoption Center</h1><!-- Note that & needs to be escaped in HTML-->
-			</div>
-			<title>J &amp; G Animal Rescue and Adoption Center</title>
-	</header>
+	<title>J &amp; G Animal Rescue and Adoption Center</title>
 </head>
 <!-- Start of page Body -->
 <body>
+	<div id="top">
+		<h1>J &amp; G Animal Rescue and Adoption Center</h1><!-- Note that & needs to be escaped in HTML-->
+	</div>
