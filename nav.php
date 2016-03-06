42c230bb8e570d5e397357f7803487a1460ab33b
Greg Boyarko <boyarkogc@gmail.com>
2016-03-06 10:58:46 -0700

    Fixed nav

--- nav.php
+++ nav.php
@@ -1,25 +1,15 @@
-  <div id = "links">
-		<nav>
-			<a href="index.php">
-				Home</a>
+<div id = "links">
+	<nav>
+		<a href="index.php">Home</a>
 
-			<a href="login.php">
-					Log in</a>
+		<a href="about.php">About us</a>
+		
+		<a href="adopt.php">Our Adoptable Animals</a>
 
-			<a href="aboutus.php">
-				About us</a>
-
-			<a href="availableanimals/index.php">
-				Our Adoptable Animals</a>
-
-			<a href="contactus.php">
-				Contact us</a>
-
-      <?php if (!isset($_SESSION['userName'])): ?>
-      <a href="login.php">Login</a>
-
-      <?php else: ?>
-      <a href="logout.php">Logout</a>
-      <?php endif; ?>
-		</nav>
-	</div>
+    	<?php if (!isset($_SESSION['userName'])): ?>
+    	<a href="login.php">Login</a>
+    	<?php else: ?>
+    	<a href="logout.php">Logout</a>
+    	<?php endif; ?>
+	</nav>
+</div>
