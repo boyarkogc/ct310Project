a43606ec24730dd6cfe837ff6c3ef011a86587b4
Citizensnips33 <Sherwood33@gmail.com>
2016-04-27 12:46:07 -0600

    Moved stuff into the right folders, and Made Awake
    Made Awake.php and moved stuff into the right folders, Its live at
    http://www.cs.colostate.edu/~wadesher/P3/

---
+++ awake.php
@@ -0,0 +1,37 @@
+	<?php
+	header('Content-Type: text/json'); 
+
+	$url = "www.cs.colostate.edu/~wadesher/P3/index.php"; //Im using Curl to get the up/down status of our index to determine if the site is up or down
+	
+     
+     $ch=curl_init();
+ 
+     // sets the URL to fetch
+     curl_setopt ($ch, CURLOPT_URL,$url );
+ 
+     // return the transfer as a string
+     curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+ 
+     // disable output verbose information
+     curl_setopt ($ch,CURLOPT_VERBOSE,false);
+ 
+     // max number of seconds to allow cURL function to execute
+     curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+ 
+     curl_exec($ch);
+ 
+     // get HTTP response code
+     $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+ 
+     curl_close($ch);
+ 	
+ 		
+     if($httpcode>=200 && $httpcode<300)
+          $Status = array("status" => "up");
+     else if($httpcode>=400 && $httpcode<500)
+           $Status = array("status" => "error");
+       else
+       	$Status = array("status" => "down"); //This means it timed out with no error so its down
+
+	echo json_encode($Status);
+?>
